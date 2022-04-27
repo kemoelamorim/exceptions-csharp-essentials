19dedb6c09ff54f5da362a00d4890c49b63e1a0d
Kemoel Miranda <kemoel.miranda@ailos.coop.br>
2022-04-26 21:56:49 -0300

    Finalizando curso de Exception

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,57 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta = new ContaCorrente(8,9);
-                ContaCorrente conta2 = new ContaCorrente(8,19);
+
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("CACTH no método MAIN");
+            }
+            Console.ReadLine();
+
+
+        }
+        private static void CarregarContas()
+        {
+
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt")) 
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //----------------------------------------
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("arquivo.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}catch (IOException)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada");
+            //}
+            //finally
+            //{
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+
+            //    }
+            //    Console.WriteLine("Executando o finally");
+            //}
+
+        }
+        private static void TestaInnerException() {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(8, 9);
+                ContaCorrente conta2 = new ContaCorrente(8, 19);
                 conta2.Transferir(10000, conta);
                 //conta.Sacar(1000);
-                
+
                 //Metodo(); 
             }
             catch (OperacaoFinanceiraException ex)
@@ -26,12 +73,8 @@ namespace ByteBank
                 Console.WriteLine("Informações na innerException: ");
                 Console.WriteLine(ex.InnerException.Message);
                 Console.WriteLine(ex.InnerException.StackTrace);
-                
+
             }
-
-            Console.ReadLine();
-
-
         }
 
         public static void Metodo()
